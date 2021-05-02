3379267bf608af8ca2459e29be7d801d39f3fd5f
Wes Waterman <wwaterman12@gmail.com>
2021-05-01 18:08:22 -0700

    :wrench: add alias to src folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+const path = require("path");
+const projectRootDir = path.resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  resolve: {
+    alias: [
+      {
+        find: "~",
+        replacement: path.resolve(projectRootDir, "src"),
+      },
+    ],
+  },
+});
