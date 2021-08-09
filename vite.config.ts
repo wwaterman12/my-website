937abb0a6bb8fa133d67815d0710e36fb6319ba1
Wes Waterman <wwaterman12@gmail.com>
2021-08-09 19:12:21 +0900

    :heavy_plus_sign: add svg loader

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import svgLoader from "vite-svg-loader";
 
 const path = require("path");
 const projectRootDir = path.resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), svgLoader()],
   resolve: {
     alias: [
       {
